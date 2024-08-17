3481f037fbb2d0935943406996407ce883912f0e
myudro33 <qanashvilinika7@gmail.com>
2024-08-17 18:58:49 +0400

    refactor: add dummy data for accordions

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,18 +1,20 @@
+import { useState } from "react";
 import AccordionIcon from "./AccordionIcon";
 import PropTypes from "prop-types";
 
-function Accordion({ active, title, answer,setActive }) {
+function Accordion({ title, answer }) {
+  const [show,setShow]= useState(false)
   return (
     <div className="w-full mt-10 ">
-      <div onClick={()=>setActive(!active)} className="flex justify-between cursor-pointer">
+      <div onClick={()=>setShow(!show)} className="flex justify-between cursor-pointer">
         <h1 className="text-dark-purple hover:text-gray-purple font-bold text-xl">
           {title}
         </h1>
-        <AccordionIcon active={active} />
+        <AccordionIcon active={show} />
       </div>
       <div
         className={`w-full bg-gray-purple grid overflow-hidden transition-all duration-300 ease-in-out ${
-          active ? "grid-rows-[1fr] opacity-100" : "grid-rows-[0fr] opacity-0"
+          show ? "grid-rows-[1fr] opacity-100" : "grid-rows-[0fr] opacity-0"
         }`}
       >
         <div className="overflow-hidden">{answer}</div>
@@ -22,9 +24,7 @@ function Accordion({ active, title, answer,setActive }) {
 }
 
 Accordion.propTypes = {
-  active: PropTypes.boolean,
   title: PropTypes.string,
   answer: PropTypes.string,
-  setActive:PropTypes.func
 };
 export default Accordion;
