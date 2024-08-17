39a389de34ea2310ba2f5d6fc5e68ebaa0d1e9b0
myudro33 <qanashvilinika7@gmail.com>
2024-08-17 16:43:46 +0400

    style: add responsive background images

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
 function App() {
   return (
-    <>
-    <h1 style={{color:'white'}}>ნიკა ქანაშვილი Nika Kanashvili</h1>
-    </>
+    <div className='w-screen h-screen'>
+      <div className='xs:hidden lg:flex'>
+        <img className='w-full' src="../public/assets/background-pattern-desktop.svg" alt="background" />
+      </div>
+      <div className='xs:flex lg:hidden'>
+        <img className='w-full' src="../public/assets/background-pattern-mobile.svg" alt="background" />
+      </div>
+    </div>
   )
 }
 
